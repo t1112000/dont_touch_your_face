fc8d62b1ce82493de34a2e31333ec23498f8fdee
Tuyen <9a5.tuyen@gmail.com>
2021-10-17 13:51:40 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+// import * as mobilenetModule from "@tensorflow-models/mobilenet";
+// import * as knnClassifier from "@tensorflow-models/knn-classifier";
+// import { Howl } from "howler";
+// import soundURL from "./assets/botayra.mp3";
+
+// const sound = new Howl({
+//   src: [soundURL],
+// });
+
+// sound.play();
 
 function App() {
+  const video = useRef();
+
+  const init = async () => {
+    console.log("init...");
+    await setupCamera();
+  };
+
+  const setupCamera = () => {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+          { video: true },
+          (stream) => {
+            video.current.srcObject = stream;
+          },
+          (error) => reject(error)
+        );
+      } else {
+        reject();
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video ref={video} className="video" src="" autoPlay />
+
+      <div className="control">
+        <button className="btn">Train 1</button>
+        <button className="btn">Train 2</button>
+        <button className="btn">Run</button>
+      </div>
     </div>
   );
 }
